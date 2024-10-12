2244e1a3f9b5176a5d3ca9ad8cbe119809cb5600
Pietro Di Bello <pierodibello@gmail.com>
2024-10-12 20:44:44 +0200

    chore(org_employee_cli): scaffold the exercise

---
+++ org_employee_cli/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Using a hash map and vectors, create a text interface 
+    // to allow a user to add employee names to a department in a company; 
+    // for example, “Add Sally to Engineering” or “Add Amir to Sales.” 
+    // Then let the user retrieve a list of all people in a department 
+    // or all people in the company by department, sorted alphabetically.
+}
