17763c4d2b7cd0eedf07d4e9edff7d50862ab2ad
Pietro Di Bello <pierodibello@gmail.com>
2024-10-19 18:11:16 +0200

    refactor: extract the parsing logic

--- org_employee_cli/src/main.rs
+++ org_employee_cli/src/main.rs
@@ -7,6 +7,7 @@
 use std::collections::HashMap;
 use std::io;
 use std::io::Write;
+use Command::{AddEmployee, Exit, Invalid};
 
 fn main() {
     let mut company = Company::new();
@@ -16,27 +17,43 @@ fn main() {
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
-        io::stdin().read_line(&mut command).expect("Cannot read command");
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Cannot read command");
         let command = command.trim();
 
-        if command.to_lowercase() == "exit" {
-            break;
-        }
-
-        let parts: Vec<&str> = command.split_whitespace().collect();
-
-        if parts[0].to_lowercase() == "add" && parts[2].to_lowercase() == "to" {
-            let employee = parts[1].to_string();
-            let department = parts[3..].join(" ");
-
-            company.add(employee.clone(), department.clone());
-            println!("{:?}", company);
-        } else {
-            println!("Invalid command {}", parts.join(" "));
-            continue;
+        match parse_command(command) {
+            AddEmployee { name, department } => {
+                company.add(name.clone(), department.clone());
+                println!("{:?}", company);
+            }
+            Exit => break,
+            Invalid => {
+                println!("Invalid command {}", command);
+                continue;
+            }
         }
     }
     println!("Goodbye!");
+}
+
+enum Command {
+    AddEmployee { name: String, department: String },
+    Exit,
+    Invalid,
+}
+
+fn parse_command(command: &str) -> Command {
+    let parts: Vec<&str> = command.split_whitespace().collect();
+    if parts[0].to_lowercase() == "add" && parts[2].to_lowercase() == "to" {
+        let name = parts[1].to_string();
+        let department = parts[3..].join(" ");
+        AddEmployee { name, department }
+    } else if command.to_lowercase() == "exit" {
+        Exit
+    } else {
+        Invalid
+    }
 }
 
 #[derive(Debug)]
@@ -45,11 +62,16 @@ struct Company {
 }
 impl Company {
     fn new() -> Self {
-        Self { employees: HashMap::new() }
+        Self {
+            employees: HashMap::new(),
+        }
     }
 
     fn add(&mut self, employee: String, department: String) {
-        self.employees.entry(department.clone()).or_default().push(employee.clone());
+        self.employees
+            .entry(department.clone())
+            .or_default()
+            .push(employee.clone());
         println!("Added {employee} to {department}");
     }
 }
